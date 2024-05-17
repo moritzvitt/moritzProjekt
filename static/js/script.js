273e9c63cb6c5059271de38dd655178a7552828d
moritzvitt <moritzelias.vitt@gmail.com>
2024-05-17 12:07:44 +0200

    navbar padding and margin not quite right

--- static/js/script.js
+++ static/js/script.js
@@ -5,3 +5,64 @@ $(document).ready(function() {
     });
   });
   
+
+  $(document).ready(function() {
+    $('.navbar-brand').mouseenter(function() { // Mouse enter
+      $('.animate-item').each(function(i) {
+        setTimeout(function() {
+          $('.animate-item').eq(i).addClass('show');
+        }, 100 * (i+1));
+      });
+    });
+  
+    $('nav.navbar').mouseleave(function() { // Mouse leave
+      $('.animate-item').removeClass('show');
+    });
+  });
+
+  $(document).ready(function() {
+    // Function to update the visibility of divs in div_values.html
+    function updateDivVisibility() {
+        ['hint', 'explanation', 'example1', 'example2', 'grammar', 'conjugation', 'translation'].forEach(function(id) {
+            var displayStyle = localStorage.getItem('display-' + id) || 'none';
+            $('#' + id).css('display', displayStyle);
+        });
+    }
+
+    // Checks if it's running on the div_values.html
+    if ($('.box').length > 0) {
+        updateDivVisibility(); // Initial visibility update
+        window.addEventListener('storage', updateDivVisibility); // Listen for changes
+    }
+
+    // Checks if it's running on the list.html
+    if ($('#tickedFieldsCheckboxes').length > 0) {
+        $('input[type="checkbox"]').change(function() {
+            var checkboxId = $(this).attr('id');
+            var targetId = checkboxId.replace('Checkbox', '');
+            var isChecked = $(this).is(':checked');
+
+            // Send a message to div_values.html through localStorage
+            localStorage.setItem('display-' + targetId, isChecked ? 'block' : 'none');
+            // Trigger an event to notify div_values.html page
+            window.dispatchEvent(new Event('storage'));
+        });
+    }
+});
+
+  // $(document).ready(function() {
+//   // Initially hide all divs
+//   $('.toggle-div').hide();
+
+//   // For each checkbox
+//   $('input[type="checkbox"]').each(function() {
+//     // Get the corresponding div id from the checkbox id
+//     var divId = '#' + this.id.replace('Checkbox', '');
+
+//     // When the checkbox state changes
+//     $(this).change(function() {
+//       // Toggle the visibility of the corresponding div
+//       $(divId).toggle(this.checked);
+//     });
+//   });
+// });
